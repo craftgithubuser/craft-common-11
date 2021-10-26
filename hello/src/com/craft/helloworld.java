02e9e26b9019ef27d316e7d78805e89fef5529b9
azaniya tilahun <azaniyayon@gmail.com>
2021-10-25 21:57:22 -0400

    hey

---
+++ hello/src/com/craft/helloworld.java
@@ -0,0 +1,13 @@
+package com.craft;
+
+public class helloworld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+
+	System.out.println("greeting from craft common 11");
+	
+	}
+
+}
