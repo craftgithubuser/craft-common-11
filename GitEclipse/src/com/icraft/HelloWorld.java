cc6226aa6140d158a0ddd9485c8b19dcd23fe202
AliAli5767 <ametelali@gmail.com>
2021-10-25 21:01:21 -0500

    first java project

---
+++ GitEclipse/src/com/icraft/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.icraft;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Greetings from craft-common-11");
+		
+	}
+
+}
