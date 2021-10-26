47aeb03fba4745aa236a7cf66ffbf66fd004eb72
Tess12345 <taltamo82@gmail.com>
2021-10-26 12:54:43 -0400

    document updated

--- Sample project/src/Giteclipseint/Gitdemo.java
+++ Sample project/src/Giteclipseint/Gitdemo.java
@@ -5,6 +5,8 @@ public class Gitdemo {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("welcome to java");
+	System.out.println("clas october");
+	
 	}
 
 }
