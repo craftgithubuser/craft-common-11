b9901f995859c7d7e15108f82db59928d75a9a1c
melak24 <melak24@gmail.com>
2021-10-25 21:56:02 -0400

    commit from Melak eclipse

---
+++ actualFirstTest/src/firstPackage/HelloWorld.java
@@ -0,0 +1,11 @@
+package firstPackage;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Greetings from Melak Alemu");
+	}
+
+}
