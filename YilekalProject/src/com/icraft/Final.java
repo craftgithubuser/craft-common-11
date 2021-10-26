fd5bfd9b19342f1f9143dba97dac5cffb3f7af3a
GebreYilekal <gebrey750@gmail.com>
2021-10-26 02:54:14 -0400

    still strong

---
+++ YilekalProject/src/com/icraft/Final.java
@@ -0,0 +1,10 @@
+package com.icraft;
+
+public class Final {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        System.out.println("welcome to the real world");
+	}
+
+}
