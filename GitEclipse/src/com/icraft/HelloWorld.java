dced88bf54d2687ac13122d1192b9f25c1a4be9f
AliAli5767 <ametelali@gmail.com>
2021-10-26 10:27:53 -0500

    first update

--- GitEclipse/src/com/icraft/HelloWorld.java
+++ GitEclipse/src/com/icraft/HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("Greetings from craft-11");
+System.out.println("Greetings from craft-common-11");
 	}
 
 }
