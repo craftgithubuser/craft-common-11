d0da1ae8811d66f326e0972e7fc55a24aec20a75
ttamerat99 <ttamerat0705@gmail.com>
2021-10-25 22:49:22 -0400

    Java project from eclipse

---
+++ Hellothere/src/Gitclass/Project1.java
@@ -0,0 +1,12 @@
+package Gitclass;
+
+public class Project1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("hello");
+		System.out.println("tselote");
+	}
+
+}
