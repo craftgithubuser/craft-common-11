7329571a9303d9e00260a43428fe5a1625a6c105
AliAli5767 <ametelali@gmail.com>
2021-10-26 10:52:55 -0500

    array.java

---
+++ GitEclipse/src/com/icraft/Array.java
@@ -0,0 +1,13 @@
+package com.icraft;
+
+public class Array {
+
+	   public static void main(String[] args) {
+	      double[] myList = {1.9, 2.9, 3.4, 3.5};
+
+	      // Print all the array elements
+	      for (double element: myList) {
+	         System.out.println(element);
+	      }
+	   }
+	}
