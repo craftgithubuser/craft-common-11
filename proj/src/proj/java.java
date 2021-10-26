c75ae0d7b6e4a96dc7f4a8965d15f117ee982b13
Tess12345 <taltamo82@gmail.com>
2021-10-26 19:29:44 -0400

    java again added

--- proj/src/proj/java.java
+++ proj/src/proj/java.java
@@ -5,6 +5,8 @@ public class java {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 System.out.println("hello java");
+	System.out.println("java again");
+	
 	}
 
 }
