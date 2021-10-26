8a29802f35b2b087c43a9a445007710b7443e361
helenkahsu <you@example.com>
2021-10-25 21:55:34 -0500

    "newclass"

---
+++ HelloWorld/src/com/icraft/Newja.java
@@ -0,0 +1,10 @@
+package com.icraft;
+
+public class Newja {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
---
+++ HelloWorld/src/com/icraft/NewjaTest.java
@@ -0,0 +1,14 @@
+package com.icraft;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class NewjaTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}
