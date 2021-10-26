bb31520ed6441b2cfb5c7df37880fb887fc59aa5
SoresaHailu <craft.soresa@gmail.com>
2021-10-25 22:04:25 -0400

    commit from eclipse

--- Project1/src/com/icraft/Example.java
+++ Project1/src/com/icraft/Example.java
@@ -1,11 +1,12 @@
 package com.icraft;
 
-public class HelloWorld {
+public class Example {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
 		System.out.println("Greeting from craft-common-11");
+		System.out.println("some change");
 	}
 
 }
