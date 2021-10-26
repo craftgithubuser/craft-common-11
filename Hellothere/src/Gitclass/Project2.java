b48e02cd2583e7fa6fdb643d697e809faf8686d9
ttamerat99 <ttamerat0705@gmail.com>
2021-10-25 23:11:43 -0400

    addition calculator added

---
+++ Hellothere/src/Gitclass/Project2.java
@@ -0,0 +1,20 @@
+package Gitclass;
+
+public class Project2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int val1 =20;
+		int val2 =10;
+		
+		int add = val1 + val2;
+		
+		
+		System.out.println("Addition of two numbers: " + add);
+
+		
+		
+	}
+
+}
