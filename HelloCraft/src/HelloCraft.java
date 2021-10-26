0b111e7865b7576b7143a99561ce2b695572d3a4
nunakibe <nkiber@montgomerycollege.edu>
2021-10-25 22:12:44 -0400

    committed

--- HelloCraft/src/HelloCraft.java
+++ HelloCraft/src/HelloCraft.java
@@ -4,6 +4,7 @@ public class HelloCraft {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Greeting from craft Common-11");
+		System.out.println(" ");
 	}
 
 }
