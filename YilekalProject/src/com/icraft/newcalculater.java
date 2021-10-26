adbed920f23778bea9d7dbff562a09eea61fe437
GebreYilekal <gebrey750@gmail.com>
2021-10-26 02:59:22 -0400

    new calculater

---
+++ YilekalProject/src/com/icraft/newcalculater.java
@@ -0,0 +1,25 @@
+package com.icraft;
+
+public class newcalculater {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int val1 =100;
+		int val2 =50;
+		
+		int add = val1 + val2;
+		int sub = val1 - val2;
+		int mult = val1 * val2;
+		int div = val1/val2;
+		
+		System.out.print("Addition of two number:"+ add);
+		System.out.print("Subtraction of two number:"+ sub);
+		System.out.print("Multiplication of two number:"+ mult);
+		System.out.print("Division of two number:"+ div);
+
+		
+		
+	}
+
+}
