0e6089bcd250f26226bf5d6626cd5cec1fcd0ef2
swondmu <swondmu@yahoo.com>
2021-10-26 20:07:32 -0400

    java files commit

---
+++ HellowWorledMultiple/src/first/GitDimo.java
@@ -0,0 +1,12 @@
+package first;
+
+public class GitDimo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		
+		System.out.print("starting an Eclipse java project");
+	}
+
+}
