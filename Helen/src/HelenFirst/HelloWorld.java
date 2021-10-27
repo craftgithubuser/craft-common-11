264041ba4202928744944600c435b370bce4cd04
Hermela1212 <helenmulugeta1215@gmail.com>
2021-10-26 20:39:05 -0400

    new helen java

---
+++ Helen/src/HelenFirst/HelloWorld.java
@@ -0,0 +1,11 @@
+package HelenFirst;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Greetings from craft-common-11");
+System.out.println("hello helen ");
+	}
+
+}
