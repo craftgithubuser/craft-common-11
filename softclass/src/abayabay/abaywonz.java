804be9891632bf4bb52fa8a8246eaf93a3858137
craftsoftclass <craftsoftclass@gmail.com>
2021-10-27 22:42:16 -0400

    text aded

--- softclass/src/abayabay/abaywonz.java
+++ softclass/src/abayabay/abaywonz.java
@@ -5,6 +5,8 @@ public class abaywonz {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println ("abay wonzachen");
+		System.out.println ("abay yegedbal");
+		
 	}
 
 }
