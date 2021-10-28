44a75acf6d579eb5572145a6ff4744a494b97a5f
TekleAbay <tekle.abaytekle@gmail.com>
2021-10-28 16:23:39 -0400

    Commit from Eclipse

---
+++ Project1/src/Pack1/Class1.java
@@ -0,0 +1,10 @@
+package Pack1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Hello october common-craft-11 batch!");
+	}
+
+}
