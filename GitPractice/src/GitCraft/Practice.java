866be859bfb507fb4542f32c432ebe14f992b1f9
AliAli5767 <ametelali@gmail.com>
2021-10-30 14:11:06 -0500

    commit for java practice

---
+++ GitPractice/src/GitCraft/Practice.java
@@ -0,0 +1,12 @@
+package GitCraft;
+
+public class Practice {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("We need to plan sometime to practice!");
+
+	}
+
+}
