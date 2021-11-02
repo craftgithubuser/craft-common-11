7abd2bb0f7e61a6eccfa0230d918ba2956556a56
ASCHALEWBEZI <asch1919@yahoo.com>
2021-11-02 17:06:26 -0400

    hello

---
+++ BBB/src/ccc/ddd.java
@@ -0,0 +1,10 @@
+package ccc;
+
+public class ddd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("greeting from aschalew for craft student");
+	}
+
+}
