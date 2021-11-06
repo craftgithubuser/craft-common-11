553444df43b0f2897414fa9871bb6521b11cf394
zaid1004 <firhiwot0517@gmail.com>
2021-11-05 20:00:03 -0700

    this is git

---
+++ helloClass/src/helloClass/Hello.java
@@ -0,0 +1,11 @@
+package helloClass;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is git demonstaration");
+
+	}
+
+}
