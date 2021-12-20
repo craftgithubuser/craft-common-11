ea09fad9b58b121be7ba74b7dc9d293f36feb280
craftgithubuser <craftknowledge.soresa@gmail.com>
2021-12-20 07:19:06 -0500

    commit from vs code

--- nani/src/nani01/craft11.java
+++ nani/src/nani01/craft11.java
@@ -4,8 +4,8 @@ public class craft11 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-System.out.println("nani message"
-		);
+System.out.println("nani message");
+System.out.println("copy that");
 	}
 
 }
